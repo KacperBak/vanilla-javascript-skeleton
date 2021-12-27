407388132ce118133c087cd670bd76dbbd61cfe7
Kacper Bak <github@kacperbak.de>
2021-12-27 22:47:50 +0100

    chartjs with bootstrap starter template

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-console.log("Hello world!");
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
 
-function pushed(){
-  let currentDate = new Date();
-  let milliSeconds = currentDate.getTime();
-  console.log("Button pushed on Date: " + currentDate);
-  console.log("Milliseconds passed since 1970: " + milliSeconds);
-}
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+console.log("Hello Chart.js");
